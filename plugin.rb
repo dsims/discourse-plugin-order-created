4545454a54239e638d91a740e5bbac490c098b79
dsims <dsims@users.noreply.github.com>
2025-04-17 09:44:58 -0400

    Initial plugin

--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 # name: discourse-plugin-order-created
-# about: TODO
-# meta_topic_id: TODO
+# about: Makes created_at the default sort order for topics
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: dsims
+# url: https://github.com/dsims/discourse-plugin-order-created
 # required_version: 2.7.0
 
 enabled_site_setting :discourse_plugin_order_created_enabled
 
-module ::DiscoursePluginOrderCreated
-  PLUGIN_NAME = "discourse-plugin-order-created"
-end
-
-require_relative "lib/discourse_plugin_order_created/engine"
-
 after_initialize do
-  # Code which should run after Rails has finished booting
+  register_modifier(
+    :topic_query_apply_ordering_result,
+  ) do |result, sort_column, sort_dir, options, topic_query|
+    if sort_column.blank? || sort_column == "default"
+      sort_column = "created_at"
+      result.order("topics.#{sort_column} #{sort_dir}")
+    end
+  end
 end
